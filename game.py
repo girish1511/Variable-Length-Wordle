9b84ffbf4570888906c5b6861390ef247cd9760e
girish1511 <girishg@umich.edu>
2022-12-16 00:28:07 -0500

    updated code

--- game.py
+++ game.py
@@ -4,10 +4,17 @@ import time
 import sys
 import utils
 
-word = "apple"
-letters = {chr(i): 0 for i in range(65,91)}
-
+#This function is the only one, among the entire code, taken from the internet and not my own.
 def getChar():
+    """
+    Accepts only a single character from the terminal as an input, and the character is read without 
+    the need to press enter.
+
+    Returns
+    -------
+    char
+        A single character read as input
+    """
     try:
         # for Windows-based systems
         import msvcrt # If successful, we are on Windows
@@ -29,23 +36,41 @@ def getChar():
         return answer
 
 def get_specific_letters(letters):
+    """
+    Function that accepts a single character provided it is present in <letters>.
+    The function keeps running until the user enters a character present in <letters>
+
+    Parameters
+    ----------
+    letters : list
+        Allowed list of characters as input
+
+    Returns
+    -------
+    char
+        Single character entered by the user and present in <letters>
+    """
     c = ''
     while(c not in letters):
         c = getChar()
     return c
 
-def display(blank_list, guesses):
-    # blank_str = ' '.join(blank_list)
-    # print(f"\n{blank_str}  Number of guesses left: {guesses}\n")
-    for i in range(len(blank_list)):
-        print(f"\r{' '.join(guesses[i])}")
-        print(f"\r{' '.join(blank_list[i])}\n")
-    return
+def display(blank_list):
+    """
+    Function that is in-charge of printing the layout in the desired format based
+    on the strings and characters present in <blank_list>
 
-# blank_list = [['_']*6]*6
-# guesses = [['q']*6]*6#['']*6
-# display(blank_list, guesses)
-# print(''.join([]))
+    Parameters
+    ----------
+    blank_list : list(list)
+        List of lists consisting of information on characters and their color to be
+        printed in the layout
+    """
+    # sys.stdout.write("\x1b[K\n"*(2*len(blank_list)+2))
+    # sys.stdout.flush()
+    for i in range(len(blank_list)):
+        print(f"\r{'  '.join(blank_list[i])}\n")
+    return
 
 def yes(prompt):
     """
@@ -69,34 +94,77 @@ def yes(prompt):
             return False
         else:
             print('Please enter a valid response!!')
-# guess_list = [*word_guess]
-def check_win(res):
-    if res.count('*') == len(res):
-        return True
-    else:
-        return False
     
 def update(word, word_guess):
-    blank_list = []
+    """
+    Function that color codes the letters based on the rules of the Wordle game.
+    Letters of <word_guess> that match and are present in the right location are
+    assigned green and letters that match but are in the wrong location are assigned
+    yellow. Two for loops are used to take care of words containing non-unique set of
+    letters. `\\033[32m` assigns green color to text in terminal and
+    `\\033[33m` assigns yellow color to text in terminal. `\\033[0m]` is to let the
+    terminal know the endpoint
+
+    Parameters
+    ----------
+    word : str
+        The ground-truth word that needs to be guessed
+    word_guess : list(char)
+        The user's guess as list of char for the ease of manipulation and updation
+
+    Returns
+    -------
+    list(list), int
+        Updated <blank_list> that contains information on the correctness of the
+        user's guess. Second argument returns the count of all the letters that
+        matched and where in the correct location
+    """
+    blank_list = ['']*len(word)
     gt_list = [*word]
-    # print(gt_list)
+    # count = 0
+    mark = [0]*len(word)
+    
     for i in range(len(word)):
         if word_guess[i] == word[i]:
-            blank_list.append('*')
+            blank_list[i] = f'\033[32m{word_guess[i].upper()}\033[0m'
             gt_list.remove(word_guess[i])
-        elif word_guess[i] in gt_list:
-            blank_list.append('#')
-            gt_list.remove(word_guess[i])
-        else:
-            blank_list.append('_')
-    return blank_list
+            mark[i] = 1
+            
+    for i in range(len(word)):
+        if not mark[i]:
+            if word_guess[i] in gt_list:
+                blank_list[i] = f'\033[33m{word_guess[i].upper()}\033[0m'
+                gt_list.remove(word_guess[i])
+            else:
+                blank_list[i] = word_guess[i].upper()
+
+    return blank_list, sum(mark)
 
 def play_new(word, tree):
+    """
+    Functions that also the user to interact with the program and play the game.
+    The number of guesses is set to be equal to the length of the word to ensure
+    a fair and fun game. <blank_list> is initialized with underscores for all the
+    letters and all the guesses. <blank_list> is updated throughout the game comparing
+    the user's input with the ground-truth value.
+
+    Parameters
+    ----------
+    word : str
+        Ground-truth value
+    tree : dict
+        Dictionary made up of letters of the words as nodes.
+
+    Returns
+    -------
+    bool
+        Boolean that gives teh conclusion of the game i.e., win or lose
+    """
     l = len(word)
     blank_list = [['_']*l]*l
-    guesses = [[]*l]*l
     num_guess = l
-    display(blank_list, guesses)
+    display(blank_list)#, guesses)
+    
     fl=0
     while(True):
         if num_guess>0:
@@ -114,93 +182,123 @@ def play_new(word, tree):
                 if len(guess)<l:
                     sys.stdout.write('\x1b[2A')
                     sys.stdout.write('\x1b[2K')
-                # else:
-                #     print('\n')
-            guesses[l - num_guess] = guess
-            res = update(word, guess)
+            res, count = update(word, guess)
             blank_list[l - num_guess] = res
-            # blank_list = [blank_list[0]]*5
-            sys.stdout.write(f'\x1b[{(3*l+2)}A')
-            # sys.stdout.write(f'\x1b[{(3*l+4)}K')
-            sys.stdout.flush()
-            display(blank_list, guesses)
-            if check_win(res):
+            sys.stdout.write(f'\x1b[{(2*l+2)}A')
+            # sys.stdout.write('\x1b[K')
+            display(blank_list)#, guesses)
+            if count==l:
                 fl=1
-                sys.stdout.write('\x1b[3k')
+                break
             else:
                 num_guess -= 1
         else:
-            if check_win(res):
+            if count==l:
                 fl=1
-        if fl:
             break
-    return
-
-# play_new('apple', {'a':1,'b':2})
-def play(word, ):
-    blank_list = ['_']*len(word)
-    guesses = len(word)
-    fl = 1            
-    while(True):
-        if guesses<=0:
-            fl = 0
-            break
-        else:
-            display(blank_list, guesses)
-            while(True):
-                word_guess = input(f"Enter your guess of a {len(word)} letter word: ")
-                if len(word_guess) == len(word):
-                    break
-                print(f"Please enter a word with exactly {len(word)} letters only !!")
-                sys.stdout.write('\x1b[2A')
-                # sys.stdout.write('\x1b[2K')
-            guesses -= 1
-            blank_list = update(word, word_guess)
-            if blank_list.count('*') == len(word):
-                fl = 1
-                break
     return fl
 
-# while(True):
-#     print("Welcome to Variable Length Wordle!\n\n")
-#     fl = play(word)
-#     if fl:
-#         print("\nCongratulations you won!!!!")
-#     else:
-#         print("\nBetter luck next time!!")
-#         print(f"The answer is {word}")
-#     if not yes("Would you like to play again!"):
-#         break
-# print("Thank you for playing the game!!")
+def intro_display():
+    """
+    Function that displays the intro for the game that includes instructions and disclaimers
+    """
+    ins = ["The possible letters displayed are to guide to enter only valid English words",
+           "\033[;32mGreen\033[0m color indicates that the letter is present in the word and at the right location",
+           "\033[;33mYellow\033[0m color indicates that the letter is present in the word but at the wrong location"]
+    disclaim = "Disclaimer: Although most of the offsensive words have been removed, some might remain! Play with caution!"
+    print("\033[4;1mWelcome to Variable Length Wordle!\033[0m".center(100), end="\n\n")
+    print("The objective of the game is to guess the word within the given number of tries!!\n".center(100))
+    print("Instructions:")
+    for i in ins:
+        print(f"- {i}")
+    print(f"\033[7;1m{disclaim}\033[0m", end='\n\n')
+    return
 
-# print("Available letters: a b d c")
-# value = input("Enter guess: ")
-# sys.stdout.write('\x1b[2A')
-# sys.stdout.write('\x1b[2K')
-# def getch(letters):
-#     for l in letters:
-#         if keyboard.is_pressed(l):
-#             return l
-# print("Enter guess:")
-# du = []
+def get_random_word(len_data):
+    """
+    Function asks the user if they want to decide the length of the word to be played. If the user
+    doesn't then the program randomly selects one. The length of the word is kept between 4 and 8
+    (both inclusive) to ensure a fun, stress-free game. Another reason for the upper limit is that
+    for some reason the limit to the number of lines allowed to move up the terminal via escape 
+    sequence is 20. This might be a local error, nevertheless, to avoid issues the upper limit is
+    set to 6.
     
-# while(True):
-#     k = getch(['a','b','c'])
-#     if k:
-#         print(k)
-#         print('yes')
-#         sys.stdout.flush()
-#         break
+    With the <word_len> decided as above, the program selected a word of length <word_len> by using
+    the <len_data> tree created at the beginning by calling <utils.get_data()> 
+
+    Parameters
+    ----------
+    len_data : dict
+        Dictionary with key: value pair as k: [word if len(word)==k]
+
+    Returns
+    -------
+    str
+        The ground-truth word that is to be guessed
+    """
+    word_len = None
+    l_lim = 4
+    u_lim = 6
+    if yes("Would you like to select how many lettered word you want to play the game with: "):
+        while(True):
+            try:
+                word_len = int(input(f"Enter how many lettered word you want to play the game with \
+                                     (between {l_lim} and {u_lim} both inclusive): "))
+            except:
+                print("Please enter a numeric value!!!")
+                
+            if l_lim<=word_len<=u_lim:
+                break
+            print('Please enter length between 3 and 8 (both inclusive)!!')
+    else:
+        print("Randomly selecting the length of the word!!!")
+        word_len = random.randint(l_lim,u_lim)
+    word = random.choice(len_data[str(word_len)])
+    return word
+
+def result_display(fl, word):
+    
+    """
+    Function to display the results of the game. Additionally to make the game more
+    informative, the program enters the definition/s of the ground-truth word. If the
+    API fails to get a definition, then a google search link is provided for the
+    convenience of the user
+    """
+    #The set of 3 lines below is used to reposition and clear the terminal for a neat
+    #display of results
 
     
-# for _ in range(6):
-#     a = ''
-#     while(a not in ['a','b']):
-#         a = getChar()
-#     print(a)
+    sys.stdout.write('\x1b[K\n'*3) #Clears the next 3 lines
+    
+    if fl:
+        print("Congratulations you won!!!!".upper().center(100), end="\n\n", flush=True)
+    else:
+        print("You lost!! Better luck next time!!".upper().center(100), end="\n\n", flush=True)
+    print(f"The word is \033[3;1m{word}\033[0m".center(100))
+    print("Definitions: ")
+    word_info = utils.get_word_info(word)
+    if word_info:
+        for d in word_info.values():
+            print(f"- {d}")
+    else:
+        print("No definition found on the API!!!")
+        print(f"Link to google search for the definition: https://www.google.com/search?q={word}")
+    print("\n")
+    
 
 if __name__=="__main__":
-    data, word_tree = utils.get_data()
-    word = 'apple'
-    game_tree = word_tree[str(len(word))].copy()
-    play_new(word, game_tree)
+    #Get the word length tree and word tree by checking
+    #if the cache is present
+    len_data, word_tree = utils.get_data()
+    
+    while(True):
+        os.system('clear')
+        intro_display()
+        word = get_random_word(len_data)
+        game_tree = word_tree[str(len(word))].copy()
+        fl = play_new(word, game_tree)
+        result_display(fl, word)
+        if not yes("Would you like to play again!: "):
+            break
+    print(f"\nThank you for playing the game!!\n")
+    text = str()
--- utils.py
+++ utils.py
@@ -5,16 +5,70 @@ import time
 
 DIR = os.path.dirname(__file__)
 TREE_PATH = os.path.join(DIR,'word_tree.json')
+WORD_LEN_PATH = os.path.join(DIR,'word_len.json')
     
-def create_tree(data, word_length=7):
+def create_tree(data):
+    """
+    Function that generates the word tree based on letters of the word as nodes
+    given a list of words
+
+    Parameters
+    ----------
+    data : list
+        List of words in English language
+
+    Returns
+    -------
+    dict
+        A tree with letters of the words as nodes and structured in nested dictionary format
+    """
     tree = {}
     for word in data:
-        if len(word) not in tree.keys():
-            tree[len(word)] = {}
-        tree[len(word)] = add_to_tree(word, tree[len(word)])
+        if word.isalpha() and word.islower():
+            if str(len(word)) not in tree.keys():
+                tree[str(len(word))] = {}
+            tree[str(len(word))] = add_to_tree(word, tree[str(len(word))])
     return tree
-                
+
+def create_len_data(data):
+    """
+    Function that creates a dictionary using words list with keys as the length of the word
+    and values as the list of words of length stated by the key
+
+    Parameters
+    ----------
+    data : list
+        List of words in English language
+
+    Returns
+    -------
+    dict
+        Dictionary with key: value pair as k: [word if len(word)==k]
+    """
+    len_data = {}
+    for word in data:
+        if word.isalpha() and word.islower():
+            if str(len(word)) not in len_data.keys():
+                len_data[str(len(word))] = []
+            len_data[str(len(word))].append(word)
+    return len_data
+               
 def add_to_tree(word, tree):
+    """
+    Recursively create nodes and update the tree with the letters of <word>
+
+    Parameters
+    ----------
+    word : str
+        Word to be added to the tree
+    tree : dict
+        Tree that needs to be updated
+
+    Returns
+    -------
+    dict
+        Updated tree that contains the word
+    """
     if len(word)==1:
         try:
             tree[word] = '.'
@@ -26,33 +80,73 @@ def add_to_tree(word, tree):
         return tree
    
 def get_word_info(word):
+    """
+    Call the FreeDictionaryAPI to get the definition of the word
+
+    Parameters
+    ----------
+    word : str
+        Word whose definition is required
+
+    Returns
+    -------
+    dict or None
+        Dictionary of definitions if the API found the word's meaning else returns None
+    """
     url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
-    resp = requests.get(url).json()[0]
-    defs = resp['meanings']
-    word_info = {f'Definition {i}': defs[i]['definitions'][0]['definition'] for i in range(len(defs))}
+    try:
+        resp = requests.get(url).json()[0]
+        defs = resp['meanings']
+        word_info = {f'Definition {i}': defs[i]['definitions'][0]['definition'] for i in range(len(defs))}
+    except:
+        word_info = None
     return word_info
 
 def get_data():
+    """
+    Function checks whether the cache files for word tree and word length tree are present
+    If the cache is not present then the tree is created else the data is loaded from cache
+
+    Returns
+    -------
+    dict, dict
+        Returns word length tree containings list of words segregated based on the length of the word
+        and word tree created with nodes as letters of the word
+    """
     #https://github.com/dwyl/english-words
     with open('words_alpha.txt','r') as f:
         data = f.read().splitlines()
     
     start = time.time()
     if os.path.exists(TREE_PATH):
-        print("Local cache found!")
+        print("Local cache for tree found!")
         print("Loading from cache...")
         with open(TREE_PATH, 'r') as f:
             tree = json.load(f)
     else:
-        print("Local cache not found!")
+        print("Local cache for tree not found!")
         print("Generating word tree...")
         tree = create_tree(data)
         with open(TREE_PATH, 'w') as f:
             json.dump(tree, f)
-    print(f"Time taken for loading data: {time.time()-start}s")
-    return data, tree
+    print(f"Time taken for loading tree data: {time.time()-start}s") 
+           
+    start = time.time()       
+    if os.path.exists(WORD_LEN_PATH):
+        print("Local cache for tree found!")
+        print("Loading from cache...")
+        with open(WORD_LEN_PATH, 'r') as f:
+            len_data = json.load(f)
+    else:
+        print("Local cache for tree not found!")
+        print("Generating word length tree...")
+        len_data = create_len_data(data)
+        with open(WORD_LEN_PATH, 'w') as f:
+            json.dump(len_data, f)
+    print(f"Time taken for loading word data: {time.time()-start}s", end="\n\n\n")
+    return len_data, tree
             
     # word_info = get_word_info('hello')
             
 # if __name__=="__main__":
-#     main()
+#     get_data()
